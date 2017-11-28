d6f08ca56bb273fc2752ae5dfdfeb244bb99b982
stefan <stefanmilic189@gmail.com>
2017-11-28 19:43:47 +0100

    boran  99.9999%

--- app/js-compiled/master-js.js
+++ app/js-compiled/master-js.js
@@ -1,28 +1,29 @@
 $(window).load(function(){
 	var $nav = $('.navigacija');//kesiranje elementa koji se ponavlja
 	var $burger = $('.burger').find('i');
-	
-	
+	var $offset = $('nav').outerHeight();
+	//caching nav height for scroll top offset
+
 	//smooth scroll
 	$('a[href^="#"]').on('click', function(event) {
 		//$('a.active').removeClass('active');
 		//$(this).addClass('active');
 		var target = $(this.getAttribute('href'));
-		
+
 		if( target.length ) {
 			event.preventDefault();
 			$('html, body').stop().animate({
-				scrollTop: target.offset().top - 50
+				scrollTop: target.offset().top - $offset
 			}, 680);
 		}
-		
+
 		//SLIDE UP NAVIGACIJE  KAD KLIKNEMO NA MOBILE LINK
 		if($( window ).width() < 1024){
 			$burger.toggleClass('fa-bars').toggleClass('fa-times');
 			$nav.toggleClass('drop-down-active');
 		}
 	});
-	
+
 	//hamburger nav
 	$burger.click(function(){
 		$burger.toggleClass('fa-bars').toggleClass('fa-times');
